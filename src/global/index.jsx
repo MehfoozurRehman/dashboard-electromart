a3d38da5831a59305d8324051ba7e0b1322a4d55
Asim Rasheed <asimrasheed442@gmail.com>
2023-07-08 09:46:01 +0500

    Revert "banner update"
    This reverts commit 0c7c593d00e6e416b85d691c88175bbb721a3da8.

--- src/global/index.jsx
+++ src/global/index.jsx
@@ -146,7 +146,7 @@ export const sidebarEnteries = [
         </defs>
       </svg>
     ),
-    name: "Banners",
-    path: "/dashboard/banners",
+    name: "Content management",
+    path: "/dashboard/content-management",
   },
 ];
--- src/screens/dashboard/Categories/add.lazy.jsx
+++ src/screens/dashboard/Categories/add.lazy.jsx
@@ -1,4 +1,4 @@
-import { Input } from "components";
+import { Input, Select, Textarea } from "components";
 
 import ImageUploaderSingle from "../../../components/ImageUploaderSingle";
 import { Link } from "router";
